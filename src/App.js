bde2afadf3f9e4f3bc67caf841b7e46b26dd84d8
Javier Gonzalez <jgromeo93@gmail.com>
2021-07-29 19:43:39 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+import Frase from './components/Frase'
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+const Boton = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  `;
+
 
 function App() {
+
+
+  // State de frases
+  const [ frase, guardarFrase] = useState({})
+
+
+
+  const consultarApi = async() => {
+
+    function getRandomInt(max) {
+      let i = Math.floor(Math.random() * max);
+      return i;
+    }
+    const api = await fetch('https://breakingbadapi.com/api/quotes')
+    const frase = await api.json();
+    let randomNumber = getRandomInt(frase.length)
+
+
+    guardarFrase(frase[randomNumber]);
+    
+    // frase.then( api => console.log(api[getRandomInt(api.length)]))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Contenedor>
+
+      <Frase
+        frase={frase}
+      ></Frase>
+      <Boton
+        onClick={consultarApi}
+      >
+        Obtener frase
+      </Boton>
+    </Contenedor>
   );
 }
 
